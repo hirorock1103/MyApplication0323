a5d055d5594146a3b55531c5e6790e150906e8be
kobayashi <kobayashi@view-bridge.co.jp>
2018-03-23 10:06:26 +0900

    テスト確認用

--- app/src/main/java/com/example/user/myapplication0323/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication0323/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] list = {"test","aaa","ddddd"};
+
+
     }
 }
